3525ca6a0a53b7c93b139a71b01f9a4b4ad548af
Thomas Morin <thomas.morin@orange.com>
2015-06-15 18:12:20 +0200

    devstack: do not run update-db on a compute node
    When the devstack plugin is run, but q-scv is not
    enabled (like on a compute node), we should not
    run xxx-db-manage.
    
    Change-Id: I6551da1020758132fd747ef119d33c3c4cb270f7

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -10,7 +10,9 @@ if [[ "$1" == "source" ]]; then
 elif [[ "$1" == "stack" && "$2" == "install" ]]; then
 	setup_develop $NETWORKING_BAGPIPE_L2_DIR
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-	bagpipe-l2-db-manage --config-file $NEUTRON_CONF --config-file /$Q_PLUGIN_CONF_FILE upgrade head
+	if is_service_enabled q-svc; then
+		bagpipe-l2-db-manage --config-file $NEUTRON_CONF --config-file /$Q_PLUGIN_CONF_FILE upgrade head
+	fi
 fi
 if [[ "$1" == "unstack" ]]; then
 	rm -f $TOP_DIR/lib/neutron_plugins/${BAGPIPE_L2_AGENT}_agent
