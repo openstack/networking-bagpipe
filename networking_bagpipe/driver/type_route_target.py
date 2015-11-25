0a02c9f9d02b5adb3d8dda0e15513d265f1435c7
Thomas Morin <thomas.morin@orange.com>
2015-11-25 17:02:54 +0100

    Add some verification methods
    + update logging code for stable/liberty
    
    Co-Author: Yannick Thomas <ythomas1.ext@orange.com>
    Change-Id: I3b5c9655492e9cc1d52132fd62db8c0e38f96d25

--- networking_bagpipe/driver/type_route_target.py
+++ networking_bagpipe/driver/type_route_target.py
@@ -25,14 +25,15 @@ from sqlalchemy.orm import exc as sa_exc
 from neutron.common import exceptions as exc
 from neutron.db import api as db_api
 from neutron.db import model_base
-
+from neutron.i18n import _LI
+from neutron.i18n import _LW
 from neutron.plugins.ml2 import driver_api as api
 from neutron.plugins.ml2.drivers import helpers
 
 LOG = log.getLogger(__name__)
 
 MIN_RT_NN = 1
-MAX_RT_NN = 65535
+MAX_RT_NN = 2 ** 16 - 1
 
 route_target_opts = [
     cfg.IntOpt('rt_asn', default=64512,
@@ -48,6 +49,35 @@ cfg.CONF.register_opts(route_target_opts, "ml2_type_route_target")
 
 # TODO(tmorin): find a clean way to not collide in the segment type namespace
 TYPE_ROUTE_TARGET = 'route_target'
+
+
+class NetworkRouteTargetRangeError(exc.NeutronException):
+    message = _("Invalid network Route Target number range: '%(rt_nn_range)s' "
+                "- '%(error)s'")
+
+    def __init__(self, **kwargs):
+        # Convert rt_nn_range tuple to 'start:end' format for display
+        if isinstance(kwargs['rt_nn_range'], tuple):
+            kwargs['rt_nn_range'] = "%d:%d" % kwargs['rt_nn_range']
+        super(NetworkRouteTargetRangeError, self).__init__(**kwargs)
+
+
+def is_valid_route_target_number(rt_nn):
+    return MIN_RT_NN <= rt_nn <= MAX_RT_NN
+
+
+def verify_route_target_number_range(rt_nn_range):
+    """Raise an exception for invalid tags or malformed range."""
+    for rt_nn in rt_nn_range:
+        if not is_valid_route_target_number(rt_nn):
+            raise NetworkRouteTargetRangeError(
+                rt_nn_range=rt_nn_range,
+                error=_("%s is not a valid Route Target number") % rt_nn)
+    if rt_nn_range[1] < rt_nn_range[0]:
+        raise NetworkRouteTargetRangeError(
+            rt_nn_range=rt_nn_range,
+            error=_("End of Route Target number range is less than start of "
+                    "Route Target number range"))
 
 
 class RouteTargetInUse(exc.InUse):
@@ -85,17 +115,13 @@ class RouteTargetTypeDriver(helpers.SegmentTypeDriver):
                 rt_nn_min, rt_nn_max = entry.split(':')
                 rt_nn_min = rt_nn_min.strip()
                 rt_nn_max = rt_nn_max.strip()
-                current_range.append((int(rt_nn_min), int(rt_nn_max)))
+                rt_nn_range = int(rt_nn_min), int(rt_nn_max)
             except ValueError as ex:
-                LOG.error(_("Invalid Route Target number range: '%(range)s' "
-                            "- %(e)s. Agent terminated!"),
-                          {'range': rt_nn_ranges, 'e': ex})
-
-        LOG.info(_("Route Target number ranges: %(range)s"),
+                raise NetworkRouteTargetRangeError(rt_nn_range=entry, error=ex)
+            verify_route_target_number_range(rt_nn_range)
+            current_range.append(rt_nn_range)
+        LOG.info(_LI("Route Target number ranges: %(range)s"),
                  {'range': current_range})
-
-    def _is_valid_route_target_number(self, rt_nn):
-        return MIN_RT_NN <= rt_nn <= MAX_RT_NN
 
     def is_partial_segment(self, segment):
         return segment.get(api.SEGMENTATION_ID) is None
@@ -107,59 +133,41 @@ class RouteTargetTypeDriver(helpers.SegmentTypeDriver):
                     "network") % segment.get(api.NETWORK_TYPE)
             raise exc.InvalidInput(error_message=msg)
 
-        segmentation_id = segment.get(api.SEGMENTATION_ID)
-        if segmentation_id is None:
-            msg = _("segmentation_id required for Route Target provider"
-                    " network")
-            raise exc.InvalidInput(error_message=msg)
-        if not self._is_valid_route_target_number(segmentation_id):
-            msg = (_("segmentation_id out of range (%(min)s through "
-                     "%(max)s)") %
-                   {'min': MIN_RT_NN,
-                    'max': MAX_RT_NN})
-            raise exc.InvalidInput(error_message=msg)
-
         for key, value in segment.items():
             if value and key not in [api.NETWORK_TYPE,
-                                     api.PHYSICAL_NETWORK,
                                      api.SEGMENTATION_ID]:
-                msg = _("%s prohibited for Route Target provider "
-                        "network") % key
+                msg = _("%s prohibited for Route Target provider \
+                        network") % key
                 raise exc.InvalidInput(error_message=msg)
 
     def reserve_provider_segment(self, session, segment):
-        segmentation_id = segment.get(api.SEGMENTATION_ID)
-        with session.begin(subtransactions=True):
-            try:
-                alloc = (session.query(RouteTargetAllocation).
-                         filter_by(rt_nn=segmentation_id).
-                         with_lockmode('update').
-                         one())
-                if alloc.allocated:
-                    raise RouteTargetInUse(rt_nn=segmentation_id)
-                LOG.debug(_("Reserving specific route target %s from pool"),
-                          segmentation_id)
-                alloc.allocated = True
-            except sa_exc.NoResultFound:
-                LOG.debug(_("Reserving specific route target %s outside pool"),
-                          segmentation_id)
-                alloc = RouteTargetAllocation(rt_nn=segmentation_id)
-                alloc.allocated = True
-                session.add(alloc)
+        filters = {}
+        rt_nn = segment.get(api.SEGMENTATION_ID)
+        if rt_nn is not None:
+                filters['rt_nn'] = rt_nn
+
+        if self.is_partial_segment(segment):
+            alloc = self.allocate_partially_specified_segment(
+                session, **filters)
+            if not alloc:
+                raise exc.NoNetworkAvailable()
+        else:
+            alloc = self.allocate_fully_specified_segment(
+                session, **filters)
+            if not alloc:
+                raise RouteTargetInUse(**filters)
+
+        return {api.NETWORK_TYPE: TYPE_ROUTE_TARGET,
+                api.PHYSICAL_NETWORK: None,
+                api.SEGMENTATION_ID: alloc.rt_nn}
 
     def allocate_tenant_segment(self, session):
-        with session.begin(subtransactions=True):
-            alloc = (session.query(RouteTargetAllocation).
-                     filter_by(allocated=False).
-                     with_lockmode('update').
-                     first())
-            if alloc:
-                LOG.debug(_("Allocating Route Target number: %(rt_nn)s"),
-                          {'rt_nn': alloc.rt_nn})
-                alloc.allocated = True
-                return {api.NETWORK_TYPE: TYPE_ROUTE_TARGET,
-                        api.PHYSICAL_NETWORK: None,
-                        api.SEGMENTATION_ID: alloc.rt_nn}
+        alloc = self.allocate_partially_specified_segment(session)
+        if not alloc:
+            return
+        return {api.NETWORK_TYPE: TYPE_ROUTE_TARGET,
+                api.PHYSICAL_NETWORK: None,
+                api.SEGMENTATION_ID: alloc.rt_nn}
 
     def release_segment(self, session, segment):
         rt_nn = segment[api.SEGMENTATION_ID]
@@ -180,7 +188,8 @@ class RouteTargetTypeDriver(helpers.SegmentTypeDriver):
                     LOG.debug(_("Releasing Route Target number %s outside "
                                 "pool"), rt_nn)
             except sa_exc.NoResultFound:
-                LOG.warning(_("Route Target number %s not found"), rt_nn)
+                LOG.warning(_LW("Route Target number %(rt_nn)s not found"),
+                            {'rt_nn': rt_nn})
 
     def _sync_route_target_allocations(self):
         """Synchronize route_target_allocations table with configured ranges"""
