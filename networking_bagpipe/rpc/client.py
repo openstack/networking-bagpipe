openstack/networking-bagpipe

commit 6c14f58d4e83b0adb6afbd6ca323d0cc27a2604f

Fix port attach/detach notifications
Only notify concerned agent (host) when port is attached/detached (Disable
notifications fanout) on correct topic

Signed-off-by: Thomas Morin <thomas.morin@orange.com>
Submitted on behalf of a third-party: Orange

Change-Id: Ie371d63e6bdd0600e86e7d31f601b61bea3d7157

networking_bagpipe/rpc/client.py
@@ -46,15 +46,12 @@ class BaGPipeAgentNotifyAPI(object):
         LOG.debug('Notify BaGPipe ML2 plugin agent %(host)s at %(topic)s '
                   'the message %(method)s with %(port_bagpipe_info)s',
                   {'host': host,
-                   'topic': '%s.%s' % (self.topic_bagpipe_update, host),
+                   'topic': self.topic_bagpipe_update,
                    'method': method,
                    'port_bagpipe_info': port_bagpipe_info})
 
         cctxt = self.client.prepare(topic=self.topic_bagpipe_update,
-                                    fanout=True)
-        # TODO(tmorin): per-host topics
-        # topic = '%s.%s' % (self.topic_bagpipe_update, host)
-        # cctxt = self.client.prepare(topic=topic, fanout=True) # fanout=False?
+                                    server=True)
         cctxt.cast(context, method, port_bagpipe_info=port_bagpipe_info)
 
     def attach_port_on_bagpipe_network(self, context, port_bagpipe_info,

commit c96cf424a0e6bd58bc2ee46b7e0ef2c0a290755c

Fix RPC destination server
Fix an issue introduced in commit 6c14f58d4e83b0adb6afbd6ca323d0cc27a2604f
(server=True, should have been server=host)

Change-Id: I5556659dd4d95ec46344e2f5dbfbc2d14ad5161e

networking_bagpipe/rpc/client.py
@@ -51,7 +51,7 @@ class BaGPipeAgentNotifyAPI(object):
                    'port_bagpipe_info': port_bagpipe_info})
 
         cctxt = self.client.prepare(topic=self.topic_bagpipe_update,
-                                    server=True)
+                                    server=host)
         cctxt.cast(context, method, port_bagpipe_info=port_bagpipe_info)
 
     def attach_port_on_bagpipe_network(self, context, port_bagpipe_info,