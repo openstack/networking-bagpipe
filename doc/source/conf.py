c5d52c16d26cd802c70e0fe983240ee406a2e256
Andreas Jaeger <aj@suse.com>
2020-05-21 20:25:22 +0200

    Switch to newer openstackdocstheme and reno versions
    Switch to openstackdocstheme 2.2.1 and reno 3.1.0 versions. Using
    these versions will allow especially:
    * Linking from HTML to PDF document
    * Allow parallel building of documents
    * Fix some rendering problems
    
    Update Sphinx version as well.
    
    Set openstackdocs_pdf_link to link to PDF file. Note that
    the link to the published document only works on docs.openstack.org
    where the PDF file is placed in the top-level html directory. The
    site-preview places the PDF in a pdf directory.
    
    Disable openstackdocs_auto_name to use 'project' variable as name.
    
    Change pygments_style to 'native' since old theme version always used
    'native' and the theme now respects the setting and using 'sphinx' can
    lead to some strange rendering.
    
    openstackdocstheme renames some variables, so follow the renames
    before the next release removes them. A couple of variables are also
    not needed anymore, remove them.
    
    See also
    http://lists.openstack.org/pipermail/openstack-discuss/2020-May/014971.html
    
    Change-Id: Iceea566b1ac539ba8204b9eb3b2f27754c0f0fe7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,9 +31,11 @@ extensions = [
 ]
 
 # openstackdocstheme options
-repository_name = 'openstack/networking-bagpipe'
-bug_project = 'networking-bagpipe'
-bug_tag = ''
+openstackdocs_repo_name = 'openstack/networking-bagpipe'
+openstackdocs_pdf_link = True
+openstackdocs_auto_name = False
+openstackdocs_bug_project = 'networking-bagpipe'
+openstackdocs_bug_tag = ''
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
@@ -57,7 +59,7 @@ add_function_parentheses = True
 add_module_names = True
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'native'
 
 # Example configuration for intersphinx: refer to the Python standard library.
 #intersphinx_mapping = {'http://docs.python.org/': None}
@@ -71,7 +73,6 @@ pygments_style = 'sphinx'
 html_static_path = ['_static']
 html_theme = 'openstackdocs'
 
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
